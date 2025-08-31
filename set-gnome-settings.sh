7f28fa91f48ad311df6d4696baece86bf5ae7184
vike256 <69812383+vike256@users.noreply.github.com>
2025-08-31 18:08:16 +0300

    dark mode, unpin apps...

--- set-gnome-settings.sh
+++ set-gnome-settings.sh
@@ -1,3 +1,9 @@
+# Set dark mode
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+# Unpin apps from dock
+gsettings set org.gnome.shell favorite-apps "[]"
+
 # Reveal week numbers in the Gnome calendar
 gsettings set org.gnome.desktop.calendar show-weekdate true
 gsettings set org.gnome.desktop.interface enable-animations false
