e472369309e167f29facddb0eb128f8a4e969860
vike256 <69812383+vike256@users.noreply.github.com>
2025-08-31 18:38:40 +0300

    update before install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+sudo dnf update --refresh
+
 ./scripts/set-gnome-settings.sh
 ./scripts/set-gnome-hotkeys.sh
 ./scripts/set-gnome-extensions.sh
