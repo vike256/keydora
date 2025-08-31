b07e358fbdc01519f29d9a64c9ce9ee74d4a9423
vike256 <69812383+vike256@users.noreply.github.com>
2025-08-31 20:02:53 +0300

    fix hotkeys

--- scripts/set-gnome-hotkeys.sh
+++ scripts/set-gnome-hotkeys.sh
@@ -36,6 +36,11 @@ gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "@as
 # Workspaces
 gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
+
+for i in {1..9}; do
+  gsettings set org.gnome.shell.keybindings switch-to-application-$i "[]"
+done
+
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
