9cefe00edf032078863207e4670ec6d8de4d780e
vike256 <69812383+vike256@users.noreply.github.com>
2025-08-31 21:30:26 +0300

    Apply nerdfont to ptyxis

--- scripts/apply-configs.sh
+++ scripts/apply-configs.sh
@@ -7,6 +7,9 @@ cp ./configs/autostart/* ~/.config/autostart
 mkdir -p ~/.config/Code/User
 cp ./configs/Code/User/settings.json ~/.config/Code/User/settings.json
 
-rm -rf ~/.config/nvim/
+rm -rf ~/.config/nvim
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
+
+mkdir -p ~/.config/ptyxis
+cp ./configs/ptyxis/* ~/.config/ptyxis/
